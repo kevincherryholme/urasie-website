3cf0866dfb47a745fb84ae43a6926729d535ed62
KevinCherryholme <kevincherryholme@gmail.com>
2021-04-25 22:57:18 -0400

    Some commit long time later.

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ AOS.init({
   var carousel = function () {
     $(".home-slider").owlCarousel({
       loop: false,
-      autoplay: true,
+      autoplay: false,
       margin: 0,
       animateOut: "fadeOut",
       animateIn: "fadeIn",
@@ -50,29 +50,6 @@ AOS.init({
         },
         1000: {
           items: 1,
-        },
-      },
-    });
-    $(".carousel-testimony").owlCarousel({
-      center: true,
-      loop: true,
-      items: 1,
-      margin: 30,
-      stagePadding: 0,
-      nav: false,
-      navText: [
-        '<span class="ion-ios-arrow-back">',
-        '<span class="ion-ios-arrow-forward">',
-      ],
-      responsive: {
-        0: {
-          items: 1,
-        },
-        600: {
-          items: 2,
-        },
-        1000: {
-          items: 3,
         },
       },
     });
